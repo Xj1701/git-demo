f5523bb518e1b3875b01e00f76af62842da8a8ba
Xj1701 <mj60763411@gmail.com>
2025-12-03 18:12:55 +0800

    fixed commit helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,5 +2,9 @@
 int main(){
     printf("this is a git repository");
     printf("this is a version ");#This is a hotifx111
+    #######################################
+    ##           0                       ##
+    ##            0                      ##
+    #######################################   
     return 0;
 }
