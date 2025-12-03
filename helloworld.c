50299cd2442063865008efb4fa9e4c352e205f43
Xj1701 <133553144+Xj1701@users.noreply.github.com>
2025-12-03 19:00:41 +0800

    Add sign1 to the output in helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -4,6 +4,7 @@ int main(){
     printf("this is a version ");#this is a fix point
     #############################
     ##      This is a sign     ##
+    ##      This is a sign1    ##
     #############################
     return 0;
 }
